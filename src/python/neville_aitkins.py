92b857b297387cf7b3e0a48dc4681ce298452bb5
Ivar Stangeby <istangeby@gmail.com>
2017-01-25 12:44:32 +0100

    Added in-place storage of new values, to save memory

--- src/python/neville_aitkins.py
+++ src/python/neville_aitkins.py
@@ -10,24 +10,21 @@ def NevAit(T, c_points, t_points):
     curve of degree p that interpolates the control points.
     """
     t = t_points
-    c = c_points
+    c = np.array(c_points, dtype=np.float64)
     p = len(c) - 1
-    previous_step = np.array(c)
     for k in range(1, p + 1):
-        temp_step = np.zeros((p+1 - k, 2))
         for j in range(0, p - k + 1):
             l_one = (t[j+k] - T) / (t[j+k] - t[j])
             l_two = (T - t[j]) / (t[j+k] - t[j])
-            temp_step[j] = l_one*previous_step[j] + l_two * previous_step[j+1]
-        previous_step = temp_step
-    return previous_step[0]
+            c[j] = l_one*c[j] + l_two * c[j+1]
+    return c[0]
 
 def demo():
     c = [(i, np.random.randint(-10, 10)) for i in range(5)]
     t = range(len(c))
 
     result = []
-    for T in np.linspace(t[0], t[-1], 1000):
+    for T in np.linspace(t[0], t[-1], 100):
         result.append(NevAit(T, c, t_points=t))
 
     plt.plot(*zip(*result))
@@ -35,5 +32,6 @@ def demo():
     plt.scatter(*zip(*c), s=100, zorder=10)
     plt.show()
 
+    
 if __name__ == "__main__":
     demo()
